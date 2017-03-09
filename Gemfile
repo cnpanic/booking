34ca14cb5481bcca7bad268489d6d0a5d6159186
Chu <cnpanic@naver.com>
2017-03-09 18:25:51 +0900

    firsT

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem 'will_paginate-bootstrap'
 gem "fog-aws"
 gem 'carrierwave'
@@ -24,6 +23,11 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
+
+gem 'awesome_print', require:"ap"
+gem 'pry'
+gem 'faker'
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/initializers/aws_code.rb
+++ config/initializers/aws_code.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJI3QGQDGP7U477SA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults
     endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
